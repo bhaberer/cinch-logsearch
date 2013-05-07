08ce4e84a91c745d27229976974f01a1b7517777
Brian Haberer <bhaberer@gmail.com>
2013-05-07 11:30:11 -0700

    first commit

---
+++ lib/cinch-logsearch.rb
@@ -0,0 +1,7 @@
+require "cinch-logsearch/version"
+
+module Cinch
+  module Logsearch
+    # Your code goes here...
+  end
+end
---
+++ lib/cinch-logsearch/version.rb
@@ -0,0 +1,5 @@
+module Cinch
+  module Logsearch
+    VERSION = "0.0.1"
+  end
+end
